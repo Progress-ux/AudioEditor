419425a84d3b6a18de97bbe567192960069702d4
progress-ux <nikita15933@mail.ru>
2026-07-11 22:00:46 +0300

    feat(scanner): add throw exception for not exists directory

--- src/services/FileScanner.cpp
+++ src/services/FileScanner.cpp
@@ -1,12 +1,22 @@
 #include "FileScanner.hpp"
+#include <filesystem>
+#include <system_error>
 
 std::vector<std::filesystem::path> FileScanner::Scan(const std::filesystem::path& directory)
 {
+    if (!std::filesystem::exists(directory) || !std::filesystem::is_directory(directory))
+    {
+        throw std::filesystem::filesystem_error(
+            "Directory not found",
+            directory,
+            std::make_error_code(std::errc::no_such_file_or_directory)
+        );
+    }
+
     std::vector<std::filesystem::path> files;
     for (const auto &entry : std::filesystem::directory_iterator(directory))
     {
-        std::string filename = entry.path().filename();
-        if (filename.ends_with(".mp3"))
+        if (entry.path().extension() == ".mp3")
         {
             files.push_back(entry.path());
         }
