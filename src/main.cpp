f4e9b917c46ad63f30387db59df9cec978be249f
progress-ux <nikita15933@mail.ru>
2026-07-13 21:16:11 +0300

    feat: add CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include "CLI/CLI.hpp"
 #include "app/App.hpp"
+
 int main(int argc, char *argv[])
 {
-    App app;
+    CLI::App parser { "Audio Editor Command Line Interface" };
+
+    std::string path;
+    parser.add_option<std::string>("--path,-p", path, "Path to the directory");
+
+    CLI11_PARSE(parser, argc, argv);
+
+    App app(path);
     app.Run();
     return 0;
 }
